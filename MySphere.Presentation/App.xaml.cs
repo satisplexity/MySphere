c7f94da9982b8c369ba4c89b9bb6dadeaa4fbaaf
satisplexity <satisplexity@gmail.com>
2026-09-09 17:16:55 +0200

    feat(presentation): implemented navigationbutton style

--- MySphere.Presentation/App.xaml.cs
+++ MySphere.Presentation/App.xaml.cs
@@ -4,5 +4,11 @@ namespace MySphere.Presentation
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var mainWindow = new Views.Windows.MainWindow();
+            mainWindow.Show();
+        }
     }
 }
---
+++ MySphere.Presentation/Controls/NavigationButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MySphere.Presentation.Controls;
+
+public class NavigationButton : RadioButton
+{
+    public static readonly DependencyProperty IconDataProperty = DependencyProperty.Register(
+        nameof(IconData),
+        typeof(Geometry),
+        typeof(NavigationButton));
+
+    public Geometry? IconData
+    {
+        get => (Geometry)GetValue(IconDataProperty);
+        set => SetValue(IconDataProperty, value);
+    }
+
+    public static readonly DependencyProperty HighlightingColorProperty = DependencyProperty.Register(
+        nameof(HighlightingColor),
+        typeof(Color),
+        typeof(NavigationButton));
+
+    public Color? HighlightingColor
+    {
+        get => (Color)GetValue(HighlightingColorProperty);
+        set => SetValue(HighlightingColorProperty, value);
+    }
+}

47224c3cfe3492d4afc63bc873432c18fb2789d0
satisplexity <satisplexity@gmail.com>
2026-09-16 23:53:22 +0200

    feat(hub): add space card control

---
+++ MySphere.Presentation/Controls/SpaceCard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MySphere.Presentation.Controls;
+
+public class SpaceCard : Button
+{
+    public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+            nameof(Title),
+            typeof(string),
+            typeof(SpaceCard));
+
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register(
+            nameof(Icon),
+            typeof(Geometry),
+            typeof(SpaceCard));
+
+    public Geometry Icon
+    {
+        get => (Geometry)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+}

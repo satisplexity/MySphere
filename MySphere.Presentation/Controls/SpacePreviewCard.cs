555772ca35c5d2c1312b5b8c849c8f0c8f07d471
satisplexity <satisplexity@gmail.com>
2026-09-17 05:17:13 +0200

    feat(hub): add spaces buttons

--- MySphere.Presentation/Controls/SpacePreviewCard.cs
+++ MySphere.Presentation/Controls/SpacePreviewCard.cs
@@ -1,7 +1,6 @@
 ﻿using MySphere.Presentation.Framework.Foundation;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 
 namespace MySphere.Presentation.Controls;
@@ -25,15 +24,6 @@ public class SpacePreviewCard : Button
     {
         this.RenderTransform = Scale;
         Content = _contentImage;
-
-        this.Effect = new DropShadowEffect()
-        {
-            BlurRadius = 10,
-            Color = Color.FromArgb(255, 20, 20, 20),
-            ShadowDepth = 0,
-            Opacity = 0.5,
-            Direction = 230
-        };
     }
 
     public void Set(BitmapSource preview, ViewModelBase space)

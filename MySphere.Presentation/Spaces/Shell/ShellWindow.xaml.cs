32260bece5fb23f3783dc5f976ff7d6915b66b90
satisplexity <satisplexity@gmail.com>
2026-09-23 22:17:38 +0200

    refactor(presentation): translate element rendering in static class method

--- MySphere.Presentation/Spaces/Shell/ShellWindow.xaml.cs
+++ MySphere.Presentation/Spaces/Shell/ShellWindow.xaml.cs
@@ -1,23 +1,19 @@
 ﻿using MySphere.Presentation.Framework.Foundation;
+using MySphere.Presentation.Utilities;
 using System.Windows.Media.Animation;
 using System.Windows.Media;
 using System.Windows.Input;
 using System.Windows;
 
-using System.Windows.Media.Imaging;
-
 namespace MySphere.Presentation.Spaces.Shell;
 
 public partial class ShellWindow : Window
 {
-    public ShellWindow()
-    {
-        InitializeComponent();
-    }
+    public ShellWindow() => InitializeComponent();
 
     public void CaptureCurrentSpace()
     {
-        PART_SpaceSwitcher.Capture(RenderElement(SpaceContent), (ViewModelBase)PART_Content.Content);
+        PART_SpaceSwitcher.Capture(BitmapRenderer.Render(SpaceContent), (ViewModelBase)PART_Content.Content);
 
         PART_Container.BeginAnimation(OpacityProperty, new DoubleAnimation()
         {
@@ -58,34 +54,6 @@ public partial class ShellWindow : Window
         shellViewModel.Navigation.ReplaceWith(viewModel);
     }
 
-    public BitmapSource RenderElement(FrameworkElement element)
-    {
-        if (element is null)
-            throw new ArgumentNullException(nameof(element));
-
-        //element.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
-        //element.Arrange(new Rect(element.DesiredSize));
-        //element.UpdateLayout();
-
-        int width = (int)Math.Ceiling(element.ActualWidth);
-        int height = (int)Math.Ceiling(element.ActualHeight);
-
-        if (width <= 0 || height <= 0)
-            throw new InvalidOperationException("Element has invalid size.");
-
-        var bitmap = new RenderTargetBitmap(
-            width,
-            height,
-            96,
-            96,
-            PixelFormats.Pbgra32);
-
-        bitmap.Render(element);
-        bitmap.Freeze();
-
-        return bitmap;
-    }
-
     private async void ToogleSwitcher(object sender, RoutedEventArgs e)
     {
         await ShowSwitcher();
@@ -93,7 +61,7 @@ public partial class ShellWindow : Window
 
     private async Task ShowSwitcher()
     {
-        PART_SpaceSwitcher.Toogle(RenderElement(SpaceContent), (ViewModelBase)PART_Content.Content);
+        PART_SpaceSwitcher.Toogle(BitmapRenderer.Render(SpaceContent), (ViewModelBase)PART_Content.Content);
     }
 
     private async void Grid_KeyDown(object sender, KeyEventArgs e)
---
+++ MySphere.Presentation/Utilities/BitmapRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+
+namespace MySphere.Presentation.Utilities;
+
+public enum RenderQuality
+{
+    Low = 48,
+    Medium = 64,
+    High = 96
+}
+
+public class BitmapRenderer
+{
+    public static BitmapSource Render(FrameworkElement element, RenderQuality quality = RenderQuality.High)
+    {
+        if (element is null)
+            throw new ArgumentNullException(nameof(element));
+
+        int width = (int)Math.Ceiling(element.ActualWidth);
+        int height = (int)Math.Ceiling(element.ActualHeight);
+
+        if (width <= 0 || height <= 0)
+            throw new InvalidOperationException("Element has invalid size.");
+
+        RenderTargetBitmap bitmap = new RenderTargetBitmap(width, height, (int)quality, (int)quality, PixelFormats.Pbgra32);
+
+        bitmap.Render(element);
+        bitmap.Freeze();
+
+        return bitmap;
+    }
+}

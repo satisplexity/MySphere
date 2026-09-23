ffdabd2c4baf16d2f9ae89577f87966b0f14b18b
satisplexity <satisplexity@gmail.com>
2026-09-23 06:36:08 +0200

    feat(presentation): add window control buttons

---
+++ MySphere.Presentation/Controls/RoundButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MySphere.Presentation.Controls;
+
+public class RoundButton : Button
+{
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register(nameof(Icon), typeof(Geometry), typeof(RoundButton));
+
+    public Geometry Icon
+    {
+        get => (Geometry)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+}
--- MySphere.Presentation/Spaces/Shell/ShellViewModel.cs
+++ MySphere.Presentation/Spaces/Shell/ShellViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MySphere.Presentation.Framework.Foundation;
 using MySphere.Presentation.Spaces.Hub;
 using MySphere.Presentation.Spaces.Shell.Navigation;
+using System.Windows;
 
 namespace MySphere.Presentation.Spaces.Shell;
 
@@ -10,10 +11,24 @@ public sealed class ShellViewModel : ViewModelBase
 
     public AsyncRelayCommand ToggleSpaceSwitcherCommand { get; }
 
+    public RelayCommand ShutdownCommand { get; }
+
+    public RelayCommand CollapseCommand { get; }
+
     public ShellViewModel(IShellNavigationService navigation)
     {
         Navigation = navigation;
 
         Navigation.NavigateTo<HubViewModel>();
+
+        ShutdownCommand = new(_ =>
+        {
+            Application.Current.Shutdown();
+        });
+
+        CollapseCommand = new(_ =>
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        });
     }
 }
